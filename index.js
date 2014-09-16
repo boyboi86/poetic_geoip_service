8816ba71a14aefd5432e84519f8227ad4e642a5e
Alvise Susmel <alvise@poeticoding.com>
2014-09-16 15:53:24 +0100

    newrelic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+var newrelic = require('newrelic');
 var express = require('express');
 var mongodb = require('mongodb');
-var newrelic = require('newrelic');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
